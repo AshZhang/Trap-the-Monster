7b2ebfe0cf8531ee8b83a2d776180e86508b0217
AshZhang <ashley.w.zhang@gmail.com>
2018-11-03 10:53:49 -0700

    Added stage, placeholder player, monster

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour {
+
+	public Collider2D radius;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float vel;
+	public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horVel = vel * Input.GetAxis ("Horizontal");
+		float verVel = vel * Input.GetAxis ("Vertical");
+		rb.velocity = new Vector2 (horVel, verVel);
+	}
+}
